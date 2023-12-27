79d0f9be6ad9137e8b4fd96dda02e7254a309b1b
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2023-12-27 11:20:42 +0700

    Dimensions

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Dimensions, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <View style={styles.box}>
+                <Text style={styles.text}>well come to comback</Text>
+            </View>
+        </View>
+    );
 }
-
+const winddowWidth = Dimensions.get("window").width;
+const winddowHeight = Dimensions.get("window").height;
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "plum",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    box: {
+        width: winddowWidth > 500 ? "70%" : "90%",
+        height: winddowHeight > 500 ? "60%" : "90%",
+        backgroundColor: "green",
+        justifyContent: "center",
+    },
+    text: {
+        textAlign: "center",
+    },
 });
