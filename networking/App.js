dc08fb59b2a3918d8bd97a503dffd626e30af37e
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2023-12-28 13:47:11 +0700

    loading page await data

--- networking/App.js
+++ networking/App.js
@@ -1,15 +1,33 @@
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
-import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import {
+    ActivityIndicator,
+    FlatList,
+    SafeAreaView,
+    StyleSheet,
+    Text,
+    View,
+} from "react-native";
 
 export default function App() {
     const [posts, setPost] = useState([]);
+    const [loading, setIsLoading] = useState(true);
     useEffect(() => {
         fetch("https://jsonplaceholder.typicode.com/posts?_limit=100")
             .then((response) => response.json())
-            .then((json) => setPost(json));
+            .then((json) => {
+                setPost(json);
+                setIsLoading(false);
+            });
     }, []);
-    console.log("render");
+    if (loading) {
+        return (
+            <SafeAreaView style={styles.loadingContainer}>
+                <ActivityIndicator size={"large"} color={"#000"} />
+                <Text style={{ textAlign: "center" }}>Loading...</Text>
+            </SafeAreaView>
+        );
+    }
     return (
         <SafeAreaView style={styles.safeContainer}>
             <View style={styles.container}>
@@ -49,5 +67,12 @@ const styles = StyleSheet.create({
     postContainer: {
         padding: 6,
         borderWidth: 1,
+    },
+    loadingContainer: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+        paddingTop: StatusBar.currentHeight,
+        justifyContent: "center",
+        alignContent: "center",
     },
 });
