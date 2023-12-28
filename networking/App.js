f0f6bc90b8036e14a339545376c036e504cb298d
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2023-12-28 13:37:36 +0700

    fetch api list post

--- networking/App.js
+++ networking/App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [posts, setPost] = useState([]);
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/posts?_limit=100")
+            .then((response) => response.json())
+            .then((json) => setPost(json));
+    }, []);
+    console.log("render");
+    return (
+        <SafeAreaView style={styles.safeContainer}>
+            <View style={styles.container}>
+                {posts?.length ? (
+                    <FlatList
+                        data={posts}
+                        renderItem={(post) => (
+                            <View style={styles.postContainer}>
+                                <Text>post: {post.item.id}</Text>
+                                <Text>{post.item.title}</Text>
+                                <Text>{post.item.body}</Text>
+                            </View>
+                        )}
+                        keyExtractor={(item, index) => index.toString()}
+                        ListHeaderComponent={<Text>List Post</Text>}
+                        ItemSeparatorComponent={
+                            <View style={{ paddingVertical: 10 }}></View>
+                        }
+                    />
+                ) : null}
+            </View>
+        </SafeAreaView>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    safeContainer: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+    },
+    container: {
+        flex: 1,
+        backgroundColor: "#f5f5f5",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    postContainer: {
+        padding: 6,
+        borderWidth: 1,
+    },
 });
