d4bc80b9133e19c868ca0a52b4a0fbb22205ee10
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2023-12-28 13:02:47 +0700

    KeyboardAvoidingView

--- tutorial_1/App.js
+++ tutorial_1/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import {
+    Image,
+    KeyboardAvoidingView,
     Platform,
     Pressable,
     SafeAreaView,
+    ScrollView,
     StyleSheet,
     Text,
     TextInput,
@@ -22,32 +25,52 @@ export default function App() {
 
     return (
         <SafeAreaView style={styles.safeContainer}>
-            <View style={styles.container}>
-                <Text style={styles.header}>Login Now</Text>
-                <View style={styles.inputForm}>
-                    <TextInput
-                        style={styles.input}
-                        placeholder="email@example.com"
-                        keyboardType="email-address"
-                        value={formData.email}
-                        name="email"
-                        onChangeText={(text) => handleChange("email", text)}
-                    />
-                    <TextInput
-                        style={styles.input}
-                        placeholder="Password"
-                        secureTextEntry
-                        value={formData.password}
-                        onChangeText={(text) => handleChange("password", text)}
-                    />
-                </View>
-                <Pressable
-                    style={styles.submitForm}
-                    onPress={() => alert("you now login in successfully")}
+            <KeyboardAvoidingView
+                style={styles.container}
+                keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 0}
+            >
+                <View
+                    style={{
+                        borderWidth: 1,
+                        borderColor: "#ddd",
+                        padding: 16,
+                        borderRadius: 10,
+                    }}
                 >
-                    <Text style={styles.submitText}>Login</Text>
-                </Pressable>
-            </View>
+                    <View>
+                        <Text style={styles.header}>Login Now</Text>
+                        <Image
+                            source={require("./assets/adaptive-icon.png")}
+                            style={styles.Image}
+                        />
+                    </View>
+                    <View style={styles.inputForm}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="email@example.com"
+                            keyboardType="email-address"
+                            value={formData.email}
+                            name="email"
+                            onChangeText={(text) => handleChange("email", text)}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Password"
+                            secureTextEntry
+                            value={formData.password}
+                            onChangeText={(text) =>
+                                handleChange("password", text)
+                            }
+                        />
+                    </View>
+                    <Pressable
+                        style={styles.submitForm}
+                        onPress={() => alert("you now login in successfully")}
+                    >
+                        <Text style={styles.submitText}>Login</Text>
+                    </Pressable>
+                </View>
+            </KeyboardAvoidingView>
         </SafeAreaView>
     );
 }
@@ -61,7 +84,8 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         alignItems: "center",
-        paddingTop: Platform.OS === "ios" ? 50 : 0,
+        // justifyContent: "center",
+        paddingTop: Platform.OS === "ios" ? 60 : 0,
         backgroundColor: "#f5f5f5",
     },
     header: {
@@ -91,5 +115,12 @@ const styles = StyleSheet.create({
     submitText: {
         color: "white",
         fontSize: 16,
+        textAlign: "center",
+    },
+    Image: {
+        width: 200,
+        height: 200,
+        alignSelf: "center",
+        marginBottom: 20,
     },
 });
