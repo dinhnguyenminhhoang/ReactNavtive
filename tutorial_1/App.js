7246bc5b1f88bb9139e24428c2a3fb87f7dd7b65
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2023-12-28 12:39:45 +0700

    example form login input

--- tutorial_1/App.js
+++ tutorial_1/App.js
@@ -1,56 +1,52 @@
 import { useState } from "react";
 import {
     Platform,
+    Pressable,
     SafeAreaView,
     StyleSheet,
-    Switch,
     Text,
     TextInput,
     View,
 } from "react-native";
 export default function App() {
-    const [darkMode, setDarkMode] = useState(false);
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+    const handleChange = (fieldName, value) => {
+        setFormData({
+            ...formData,
+            [fieldName]: value,
+        });
+    };
+
     return (
-        <SafeAreaView
-            style={[
-                styles.safeContainer,
-                {
-                    backgroundColor: darkMode ? "#f5f5f5" : "#333",
-                },
-            ]}
-        >
-            <View
-                style={[
-                    styles.container,
-                    {
-                        backgroundColor: darkMode ? "#f5f5f5" : "#333",
-                    },
-                ]}
-            >
-                <TextInput
-                    style={styles.input}
-                    placeholder="text input"
-                    // secureTextEntry
-                    keyboardType="default"
-                    autoCorrect
-                    // autoCapitalize="none"
-                />
-                <TextInput
-                    style={styles.input}
-                    placeholder="text input"
-                    // secureTextEntry
-                    keyboardType="default"
-                    autoCorrect
-                    autoCapitalize="none"
-                    multiline
-                />
-                <View style={styles.swichCntainer}>
-                    <Text style={styles.swichText}>Dark mode</Text>
-                    <Switch
-                        value={darkMode}
-                        onChange={() => setDarkMode(!darkMode)}
+        <SafeAreaView style={styles.safeContainer}>
+            <View style={styles.container}>
+                <Text style={styles.header}>Login Now</Text>
+                <View style={styles.inputForm}>
+                    <TextInput
+                        style={styles.input}
+                        placeholder="email@example.com"
+                        keyboardType="email-address"
+                        value={formData.email}
+                        name="email"
+                        onChangeText={(text) => handleChange("email", text)}
+                    />
+                    <TextInput
+                        style={styles.input}
+                        placeholder="Password"
+                        secureTextEntry
+                        value={formData.password}
+                        onChangeText={(text) => handleChange("password", text)}
                     />
                 </View>
+                <Pressable
+                    style={styles.submitForm}
+                    onPress={() => alert("you now login in successfully")}
+                >
+                    <Text style={styles.submitText}>Login</Text>
+                </Pressable>
             </View>
         </SafeAreaView>
     );
@@ -60,23 +56,40 @@ const styles = StyleSheet.create({
     safeContainer: {
         flex: 1,
         paddingTop: 20,
+        backgroundColor: "#f5f5f5",
     },
     container: {
         flex: 1,
         alignItems: "center",
         paddingTop: Platform.OS === "ios" ? 50 : 0,
+        backgroundColor: "#f5f5f5",
+    },
+    header: {
+        color: "blue",
+        fontSize: 24,
+        fontWeight: "bold",
+        marginBottom: 8,
+    },
+    inputForm: {
+        marginBottom: 10,
     },
     input: {
-        height: 40,
-        width: "100%",
-        margin: 12,
-        padding: 10,
+        borderWidth: 1,
+        paddingVertical: 8,
+        paddingHorizontal: 12,
+        marginVertical: 6,
+        borderRadius: 8,
+        minWidth: 240,
+    },
+    submitForm: {
+        paddingVertical: 8,
+        paddingHorizontal: 16,
+        backgroundColor: "blue",
+        borderRadius: 10,
         borderWidth: 1,
     },
-    swichCntainer: {
-        flexDirection: "row",
-        alignContent: "center",
-        justifyContent: "center",
-        alignItems: "center",
+    submitText: {
+        color: "white",
+        fontSize: 16,
     },
 });
